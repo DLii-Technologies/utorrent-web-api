20790463f63dbb6b2412332a4b86648a37d117ce
David Ludwig <davidludwigii@gmail.com>
2019-01-05 13:12:28 -0600

    Initial commit

---
+++ src/utorrent.ts
@@ -0,0 +1,8 @@
+import { Status } from "./types";
+
+// BaseUri = "http://[IP]:[PORT]/gui/";
+// `http://[IP]:[PORT]/gui/?action=[ACTION]&hash=[TORRENT HASH 1]&hash=[TORRENT HASH 2]&...`
+
+export class uTorrent {
+
+}
